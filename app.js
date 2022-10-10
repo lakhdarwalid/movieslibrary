05bc81ec64860d0200505ee088bd772fc055a311
Lakhdar <80343325+lakhdarwalid@users.noreply.github.com>
2022-10-10 00:39:41 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var moviesApp = angular.module('moviesApp', ['ngRoute']);
+
+moviesApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/home',{
+            templateUrl : 'views/home.html'
+        })
+        .when('/moviesList', {
+            templateUrl : 'views/moviesList.html',
+            controller : 'moviesController'
+        })
+        .otherwise({
+            redirectTo : '/home'
+        })
+}]);
+
+moviesApp.controller('moviesController',function($scope, $http){
+   $scope.message = "hey"
+    $http.get('http://127.0.0.1:5501/assets/movies.json').then(function(data){
+            //$scope.mydata = data;
+            console.log(data.data);
+            $scope.movies = data.data;
+    });
+});
